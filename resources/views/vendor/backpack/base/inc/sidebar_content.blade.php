7b5ab6abfce5feaddeb83055211b3cbe35753c5b
Ramadhanu-Lenovo <satriorama1233@gmail.com>
2021-05-19 10:25:04 +0700

    added: Position module to HRD

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,12 +1,12 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('reportabsence') }} "><i class="las la-calendar-day"></i> Absen Harian</a></li>
+<li class='nav-item'><a class="nav-link" href='{{ backpack_url('reportmonthabsence') }}'><i class="lar la-calendar-alt"></i> Absen Bulanan</a></li>
 @if (backpack_user()->role == 'admin')
     <li class="nav-item"><a class="nav-link" href='{{ backpack_url('role') }}'><i class="las la-user-lock"></i> Roles</a></li>
     <li class="nav-item"><a class="nav-link" href='{{ backpack_url('user') }}'><i class="las la-user"></i> Users</a></li>
-    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('position') }}'><i class="las la-user-tie"></i> Positions</a></li>
 @endif
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('reportabsence') }} "><i class="las la-calendar-day"></i> Absen Harian</a></li>
-<li class='nav-item'><a class="nav-link" href='{{ backpack_url('reportmonthabsence') }}'><i class="lar la-calendar-alt"></i> Absen Bulanan</a></li>
 @if (backpack_user()->role != 'staff')
+    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('position') }}'><i class="las la-user-tie"></i> Positions</a></li>
     <li class='nav-item'><a class='nav-link' href='{{ backpack_url('employee') }}'><i class="las la-users"></i> Karyawan</a></li>
 @endif
